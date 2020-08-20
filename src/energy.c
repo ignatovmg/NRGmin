e5c06e8418fd961a440d8d3783e205048138721d
ignatovmg <mikhail.ignatov@stonybrook.edu>
2020-08-19 20:56:09 -0400

    Fixed terms scaling in accordance to Amber

--- src/energy.c
+++ src/energy.c
@@ -19,6 +19,10 @@ lbfgsfloatval_t energy_func(
         const int array_size,
         __attribute__((unused)) const lbfgsfloatval_t step) {
 
+    static const double scale_vdw_s03 = 1.0;
+    static const double scale_coul_s03 = 0.8333333;
+    static const double eeps = 80.0;
+
     lbfgsfloatval_t total_energy = 0.0;
     lbfgsfloatval_t term_energy;
 
@@ -49,20 +53,19 @@ lbfgsfloatval_t energy_func(
         total_energy += term_energy;
     }
 
-    double ele_eps = 1.0;
     if (energy_prm->eleng) {
         term_energy = 0.0;
-        eleng(energy_prm->ag, ele_eps, &term_energy, energy_prm->ag_setup->nblst);
+        eleng(energy_prm->ag, eeps, &term_energy, energy_prm->ag_setup->nblst);
         json_object_set_new(energy_dict, "eleng", json_real(term_energy));
         total_energy += term_energy;
     }
 
     if (energy_prm->elengs03) {
         term_energy = 0.0;
-        elengs03(1.0,
+        elengs03(scale_coul_s03,
                 energy_prm->ag_setup->nblst->nbcof,
                 energy_prm->ag,
-                ele_eps,
+                eeps,
                 &term_energy,
                 energy_prm->ag_setup->nf03,
                 energy_prm->ag_setup->listf03);
@@ -80,7 +83,7 @@ lbfgsfloatval_t energy_func(
     if (energy_prm->vdw03) {
         term_energy = 0.0;
         vdwengs03(
-                1.0,
+                scale_vdw_s03,
                 energy_prm->ag_setup->nblst->nbcof,
                 energy_prm->ag, &term_energy,
                 energy_prm->ag_setup->nf03,
