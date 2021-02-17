86a1ae3423400b93f25dc7aab96a51656e721111
Thu Nguyen <ttnnguyen@cs.stonybrook.edu>
2021-02-17 14:48:00 -0500

    freeze atom

---
+++ build/test/CMakeFiles/test_all.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_all.dir/test_all.c.o"
+  "CMakeFiles/test_all.dir/__/src/setup.c.o"
+  "CMakeFiles/test_all.dir/__/src/utils.c.o"
+  "CMakeFiles/test_all.dir/__/src/energy.c.o"
+  "CMakeFiles/test_all.dir/__/parse_options.c.o"
+  "test_all.pdb"
+  "test_all"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_all.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/test/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/thu/Downloads/nrgmin/test
+# Build directory: /home/thu/Downloads/nrgmin/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_all "/home/thu/Downloads/nrgmin/build/test/test_all")
